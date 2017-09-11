a937d6acd4e802a56a562b20478b37d00943455e
a.martishin <a.martishin@corp.mail.ru>
2017-09-11 17:31:19 +0300

    bst

---
+++ basic_algorithms/bst.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+template <typename T>
+class BST {
+private:
+    struct Node {
+        T data;
+        Node *left;
+        Node *right;
+
+        Node() {
+            left = nullptr;
+            right = nullptr;
+        }
+
+        Node (T data, Node *left = nullptr, Node *right = nullptr)
+           : data(data), left(left), right(right) {}
+    };
+    Node *root;
+
+    Node* clear(Node *n) {
+        if (n == nullptr) {
+            return nullptr;
+        }
+        clear(n->left);
+        clear(n->right);
+        return nullptr;
+    }
+
+    Node* insert(T data, Node *n) {
+        if (n == nullptr) {
+            n = new Node(data);
+        } else if (data < n->data) {
+            n->left = insert(data, n->left);
+        } else if (data > n->data) {
+            n->right = insert(data, n->right);
+        }
+
+        return n;
+    }
+
+    Node* findMin(Node *n) {
+        if (n == nullptr) {
+            return nullptr;
+        } else if (n->left == nullptr) {
+            return n;
+        } else {
+            return findMin(n->left);
+        }
+    }
+
+    Node* findMax(Node *n) {
+        if (n == nullptr) {
+            return nullptr;
+        } else if (n->right == nullptr) {
+            return n;
+        } else {
+            return findMax(n->right);
+        }
+    }
+
+    Node* erase(T data, Node *n) {
+        if (n == nullptr) {
+            return nullptr;
+        } else if (data < n->data) {
+            n->left = erase(data, n->left);
+        } else if (data > n->data) {
+            n->right = erase(data, n->right);
+        } else if (n->left && n->right) {
+            Node *ptr = findMin(n->right);
+            n->data = ptr->data;
+            n->right = erase(n->data, n->right);
+        } else {
+            Node *ptr = n;
+            if (n->left == nullptr) {
+                n = n->right;
+            } else if (n->right == nullptr) {
+                n = n->left;
+            }
+            delete ptr;
+        }
+        return n;
+    }
+
+    Node* find(T data, Node *n) {
+        if (n == nullptr) {
+            return nullptr;
+        } else if (data < n->data) {
+            return find(data, n->left);
+        } else if (data > n->data) {
+            return find(data, n->right);
+        } else {
+            return n;
+        }
+    }
+
+
+    void inorder(vector<T> &nodes, Node *n) {
+        if (n != nullptr) {
+            inorder(nodes, n->left);
+            nodes.push_back(n->data);
+            inorder(nodes, n->right);
+        }
+    }
+
+
+public:
+    BST() {
+        root = nullptr;
+    }
+
+    ~BST() {
+        root = clear(root);
+    }
+
+    void insert(T data) {
+        root = insert(data, root);
+    }
+
+    void erase(T data) {
+        root = erase(data, root);
+    }
+
+    void print() {
+        vector<T> nodes;
+        inorder(nodes, root);
+
+        for (size_t i = 0; i < nodes.size(); ++i) {
+            cout << nodes[i] << ' ';
+        }
+
+        cout << endl;
+    }
+
+    bool find(T data) {
+        if (find(data, root) != nullptr) {
+            return true;
+        }
+        return false;
+    }
+
+
+};
+
+int main() {
+    BST<int> bst;
+
+    bst.insert(1);
+    bst.insert(2);
+    bst.insert(3);
+    bst.insert(4);
+    bst.insert(5);
+
+    bst.erase(2);
+
+    bst.print();
+
+    cout << bst.find(2) << endl;
+}
