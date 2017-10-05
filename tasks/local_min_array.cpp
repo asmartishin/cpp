b2af0c643d4b0efcf56786d83234acc555d565ef
a.martishin <a.martishin@corp.mail.ru>
2017-10-05 17:47:29 +0300

    local min

---
+++ tasks/local_min_array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int local_min(vector<int> &v, int l, int r) {
+    int m = l + r / 2;
+
+    if (m - 1 > 0 && m + 1 < v.size()) {
+        if (v[m - 1] > v[m] && v[m + 1] > v[m])
+            return v[m];
+        else if (v[m - 1] < v[m])
+            return local_min(v, l, m + 1);
+        else
+            return local_min(v, m, r);
+    } else {
+        return v[m];
+    }
+}
+
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (size_t i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    cout << local_min(v, 0, n) << endl;
+}
