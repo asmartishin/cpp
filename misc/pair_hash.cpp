462db7cf481c4fa0eab2b888edc46fb587175d4d
a.martishin <a.martishin@corp.mail.ru>
2017-11-06 21:08:54 +0300

    PairHash -> TPairHash

--- misc/pair_hash.cpp
+++ misc/pair_hash.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_set>
 #include <functional>
+#include <string>
 
 
 //namespace std {
@@ -13,7 +14,7 @@
 //}
 
 
-struct PairHash {
+struct TPairHash {
     template<typename T1, typename T2>
     inline size_t operator()(const std::pair<T1, T2> &p) const {
         return std::hash<T1>()(p.first) ^ std::hash<T2>()(p.second);
@@ -22,8 +23,8 @@ struct PairHash {
 
 
 int main() {
-    std::unordered_set<std::pair<int, double>, PairHash > s;
-    std::pair<int, int> p = std::make_pair(1, 2.2);
+    std::unordered_set<std::pair<int, std::string>, TPairHash > s;
+    std::pair<int, std::string> p = std::make_pair(1, "asd");
 
     s.insert(p);
 
