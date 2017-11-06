bf5573f3affb0ceb19b5b9d9224f306723f85ca7
a.martishin <a.martishin@corp.mail.ru>
2017-11-06 21:05:11 +0300

    added template parameters for pair hash function

--- misc/pair_hash.cpp
+++ misc/pair_hash.cpp
@@ -14,15 +14,16 @@
 
 
 struct PairHash {
-    inline size_t operator()(const std::pair<int, int> &p) const {
-        return std::hash<int>()(p.first) ^ std::hash<int>()(p.second);
+    template<typename T1, typename T2>
+    inline size_t operator()(const std::pair<T1, T2> &p) const {
+        return std::hash<T1>()(p.first) ^ std::hash<T2>()(p.second);
     }
 };
 
 
 int main() {
-    std::unordered_set<std::pair<int, int>, PairHash > s;
-    std::pair<int, int> p = std::make_pair(1, 2);
+    std::unordered_set<std::pair<int, double>, PairHash > s;
+    std::pair<int, int> p = std::make_pair(1, 2.2);
 
     s.insert(p);
 
