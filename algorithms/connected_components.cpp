4d582338af952bdf678747c6c95175e5c7a4064d
a.martishin <a.martishin@corp.mail.ru>
2017-11-09 20:26:06 +0300

    connected_components

--- algorithms/connected_components.cpp
+++ algorithms/connected_components.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-void dfs(vector<vector<int> > &adj, set<int> &nodes, int x, int n) {
+void dfs(vector<vector<int> > &graph, set<int> &nodes, int x, int n) {
     vector<char> visited(n);
     stack<int> s;
     s.push(x);
@@ -14,17 +14,17 @@ void dfs(vector<vector<int> > &adj, set<int> &nodes, int x, int n) {
     while (!s.empty()) {
         int v = s.top();
         s.pop();
-        for (size_t i = 0; i < adj[v].size(); ++i) {
-            if (!visited[adj[v][i]]) {
-                s.push(adj[v][i]);
-                visited[adj[v][i]] = true;
+        for (size_t i = 0; i < graph[v].size(); ++i) {
+            if (!visited[graph[v][i]]) {
+                s.push(graph[v][i]);
+                visited[graph[v][i]] = true;
             }
         }
         nodes.erase(v);
     }
 }
 
-int number_of_components(vector<vector<int> > &adj, int n) {
+int number_of_components(vector<vector<int> > &graph, int n) {
     int res = 0;
     set<int> nodes;
     for (size_t i = 0; i < n; ++i) {
@@ -32,7 +32,7 @@ int number_of_components(vector<vector<int> > &adj, int n) {
     }
     while (!nodes.empty()) {
         int x = *nodes.begin();
-        dfs(adj, nodes, x, n);
+        dfs(graph, nodes, x, n);
         ++res;
     }
     return res;
@@ -41,13 +41,13 @@ int number_of_components(vector<vector<int> > &adj, int n) {
 int main() {
     size_t n, m;
     cin >> n >> m;
-    vector<vector<int> > adj(n, vector<int>());
+    vector<vector<int> > graph(n, vector<int>());
     for (size_t i = 0; i < m; ++i) {
         int x, y;
         cin >> x >> y;
-        adj[x - 1].push_back(y - 1);
-        adj[y - 1].push_back(x - 1);
+        graph[x - 1].push_back(y - 1);
+        graph[y - 1].push_back(x - 1);
     }
 
-    cout << number_of_components(adj, n);
+    cout << number_of_components(graph, n);
 }
