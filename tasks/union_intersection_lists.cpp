1ee847cb25a0e4e8a7e578b91eff721251f12f11
a.martishin <a.martishin@corp.mail.ru>
2017-11-15 13:35:21 +0300

    intersection of lists

--- tasks/union_intersection_lists.cpp
+++ tasks/union_intersection_lists.cpp
@@ -30,6 +30,9 @@ vector<int> intersectionOfLists(vector<int> &v1, vector<int> &v2) {
     vector<int> result;
     int j = 0;
     for (size_t i = 0; i < v1.size(); ++i) {
+        if (j == v2.size() - 1)
+            break;
+
         while (j < v2.size() && v1[i] > v2[j])
             ++j;
 
