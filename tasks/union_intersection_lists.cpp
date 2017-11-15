a2db5fad9cd8443ae48f8b8c7ff4fd798d97e305
Alex Martishin <asmartishin@gmail.com>
2017-11-15 13:36:03 +0300

    Update union_intersection_lists.cpp

--- tasks/union_intersection_lists.cpp
+++ tasks/union_intersection_lists.cpp
@@ -29,6 +29,7 @@ vector<int> unionOfLists(vector<int> &v1, vector<int> &v2) {
 vector<int> intersectionOfLists(vector<int> &v1, vector<int> &v2) {
     vector<int> result;
     int j = 0;
+    
     for (size_t i = 0; i < v1.size(); ++i) {
         if (j == v2.size() - 1)
             break;
