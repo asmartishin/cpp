68ec93f7cf2f5394aca3b5cf9e0f80ecf410fc16
Alex Martishin <asmartishin@gmail.com>
2018-05-27 15:10:07 +0300

    dsu

--- algorithms/dsu.cpp
+++ algorithms/dsu.cpp
@@ -3,63 +3,62 @@
 
 using namespace std;
 
-
-void MakeSet(vector<int> &parent, vector<int> &rank, int a) {
-    parent[a] = a;
-    rank[a] = 0;
-}
-
-
-int Find(vector<int> &parent, int a) {
-    while (a != parent[a])
-        a = parent[a];
-    return a;
-}
-
-
-void Union(vector<int> &parent, vector<int> &rank, int a, int b) {
-    a = Find(parent, a);
-    b = Find(parent, b);
-
-    if (a == b)
-        return;
-
-    if (rank[a] > rank[b])
-        parent[b] = a;
-    else {
-        parent[a] = b;
-        if (rank[a] == rank[b])
-            ++rank[b];
+class TUnionSet {
+public:
+    TUnionSet(const size_t size) {
+        Parent.resize(size);
+        Rank.resize(size);
     }
-}
 
-
-void PrintDSU(vector<int> &parent, vector<int> &rank) {
-    for (size_t i = 1; i < parent.size(); ++i) {
-        cout << i << ' ' << parent[i] << ' ' << rank[i] << endl;
+    void MakeSet(const int element) {
+        Parent[element] = element;
+        Rank[element] = 0;
     }
-    cout << endl;
-}
+
+    int Find(int element) {
+        while (element != Parent[element])
+            element = Parent[element];
+        return element;
+    }
+
+    void Union(const int firstElement, const int secondElement) {
+        auto firstElementParent = Find(firstElement);
+        auto secondElementParent = Find(secondElement);
+
+        if (firstElementParent == secondElementParent)
+            return;
+
+        if (Rank[firstElementParent] > Rank[secondElementParent])
+            Parent[secondElementParent] = firstElementParent;
+        else {
+            Parent[firstElementParent] = secondElementParent;
+            if (Rank[firstElementParent] == Rank[secondElementParent])
+                ++Rank[secondElementParent];
+        }
+    }
+
+    void Print() {
+        for (size_t i = 0; i < Parent.size(); ++i) {
+            cout << i << ' ' << Parent[i] << endl;
+        }
+    }
+private:
+    vector<int> Parent;
+    vector<int> Rank;
+};
 
 
 int main() {
-    vector<int> parent(13);
-    vector<int> rank(13);
+    TUnionSet unionSet(3);
 
-    for (size_t i = 1; i <= 12; ++i)
-        MakeSet(parent, rank, i);
+    for (size_t i = 0; i < 3; ++i)
+        unionSet.MakeSet(i);
 
-    Union(parent, rank, 2, 10);
-    Union(parent, rank, 7, 5);
-    Union(parent, rank, 6, 1);
-    Union(parent, rank, 3, 4);
-    Union(parent, rank, 5, 11);
-    Union(parent, rank, 7, 8);
-    Union(parent, rank, 7, 3);
-    Union(parent, rank, 12, 2);
-    Union(parent, rank, 9, 6);
+    unionSet.Union(0, 2);
+    unionSet.Union(1, 2);
+    unionSet.Union(0, 1);
 
-    PrintDSU(parent, rank);
+    unionSet.Print();
 
     return 0;
 }
